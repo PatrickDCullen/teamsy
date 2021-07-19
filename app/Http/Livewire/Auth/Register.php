f17ddd52f5f76d9cf977c55f34e804c452430493
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-19 11:17:15 -0400

    End Ep 6: Use Livewire with Blade Components

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -26,10 +26,10 @@ class Register extends Component
     public function register()
     {
         $this->validate([
-            'name' => ['required'],
-            'companyName' => ['required', 'string', 'unique:tenants,name'],
+            'name' => ['required', 'string', 'min:20'],
+            'companyName' => ['required', 'string', 'unique:tenants,name', 'max:3'],
             'email' => ['required', 'email', 'unique:users'],
-            'password' => ['required', 'min:8'],
+            'password' => ['required', 'min:8', 'numeric'],
         ]);
 
         $tenant = Tenant::create([
@@ -49,6 +49,11 @@ class Register extends Component
         Auth::login($user, true);
 
         redirect(route('home'));
+    }
+
+    public function updated($value)
+    {
+        $this->resetErrorBag($value);
     }
 
     public function render()
